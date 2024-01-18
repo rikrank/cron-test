0c8a93741852e9dc6a9a96d7f2d47b4cbd4d1183
rikrank <morishita-r@tam-tam.co.jp>
2024-01-18 12:17:16 +0900

    misc：コミットを統合

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          {/* Get started by editing&nbsp; */}
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
         <div>
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            {/* By{' '} */}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -46,9 +46,9 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
+          {/* <h2>
             Docs <span>-&gt;</span>
-          </h2>
+          </h2> */}
           <p>Find in-depth information about Next.js features and API.</p>
         </a>
 
@@ -91,5 +91,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
